fc6d234775f5fcf9144fb3f811ff8ea3acdd17fc
ahjoy2022 <97654215+ahjoy2022@users.noreply.github.com>
2025-07-25 23:16:19 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["nas.ai7777.top"];
+pub const RS_PUB_KEY: &str = "xl0XIVijXw49ELUSv0q8XhUVggGh5prcT7bF0N7VBw4";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
